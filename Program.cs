ee4c507a8a6d4ed1a7a80d419b3ae7f301f1f81b
James Peach <jpeach6@bloomberg.net>
2018-09-05 23:28:33 +0100

    initial-commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace simple_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
